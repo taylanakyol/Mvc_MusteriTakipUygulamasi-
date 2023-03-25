6119bf8243a667044089bba16c648468d38dcda1
taylanakyol <taylan.akyoll@gmail.com>
2023-03-25 18:20:43 +0300

    Add project files.

---
+++ Mvc_MusteriTakipUygulaması/Controllers/MusteriOzetController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mvc_MusteriTakipUygulaması.Controllers
+{
+    public class MusteriOzetController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Mvc_MusteriTakipUygulaması/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mvc_MusteriTakipUygulaması.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
